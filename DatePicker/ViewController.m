c4f58d887760a828e69891cc4f385ae354296376
steve0503 <leehr76@gmail.com>
2014-01-06 13:21:09 +0900

    DatePicker
    Done

--- DatePicker/ViewController.m
+++ DatePicker/ViewController.m
@@ -9,10 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (strong, nonatomic) IBOutlet UITextField *yearField;
+@property (strong, nonatomic) IBOutlet UITextField *monthField;
+@property (strong, nonatomic) IBOutlet UITextField *dayField;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)pickerChanged:(id)sender {
+    
+    NSLog(@"pickerChanged~!!");
+    NSDate *selectedDate = self.datePicker.date;
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger flag = NSYearCalendarUnit | NSMonthCalendarUnit|NSDayCalendarUnit;
+    NSDateComponents *comp = [calendar components:flag fromDate:selectedDate];
+    self.yearField.text = [NSString stringWithFormat:@"% ld",[comp year]];
+    self.monthField.text = [NSString stringWithFormat:@"%ld",[comp month]];
+    self.dayField.text = [NSString stringWithFormat:@"%ld",[comp day]];
+}
+
 
 - (void)viewDidLoad
 {
